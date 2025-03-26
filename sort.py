7814ef205d6fc7de4f49a1bd7f359fa1d80b4940
nathan-j-lee <njlee326@gmail.com>
2025-03-26 15:09:38 -0700

    Write sorted artists to json file

--- sort.py
+++ sort.py
@@ -8,17 +8,23 @@ with open("output.json", 'r') as file:
 desired_genre = input("Enter the genre you are looking for: ").strip().lower()
 
 # Filter and display artists that match the genre
-matching_artists = []
+matching_artists = {"artists": []}
 
 for artist in data.get("artists", []):
     genres = artist.get("genres", [])
     if any(desired_genre in genre.lower() for genre in genres):
-        matching_artists.append(artist["name"])
+        # grab name
+        name = artist.get("name")
+        # genres already grabbed
+        #grab top tracks
+        top_tracks = artist.get("top_tracks")
+        artist_dict = {
+            "name": name,
+            "genres": genres,
+            "top_tracks": top_tracks
+        }
+        matching_artists["artists"].append(artist_dict)
 
-# Display results
-if matching_artists:
-    print("\nArtists matching your genre:")
-    for artist in matching_artists:
-        print(f"- {artist}")
-else:
-    print("No artists found for this genre.")
+
+with open("sorted_output.json", 'w') as file:
+    json.dump(matching_artists, file, indent=4)
